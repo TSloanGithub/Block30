062121efa2ff17bd5df8289799635d639b546cf1
Tyler Sloan <tcsloan44@gmail.com>
2024-05-28 22:35:56 -0400

    added more info about AddAllBooks function

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,1 +1,29 @@
-/* TODO - add your code to create a functional React component that displays all of the available books in the library's catalog. Fetch the book data from the provided API. Users should be able to click on an individual book to navigate to the SingleBook component and view its details. */
+/* TODO - add your code to create a functional React component that displays all of the available books in the library's catalog. 
+Fetch the book data from the provided API. Users should be able to click on an individual book to navigate to the SingleBook component and view its details. */
+import React from "react";
+import{ useState } from 'react'
+
+
+async function AddAllBooks(){
+    try{
+        const response = await fetch('https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books');
+        const result = await response.json();
+        console.log(result);
+
+    } catch(e){
+        console.error(e);
+        console.error('Error retrieving all books in the library')
+    }
+}
+
+async function GetSingleBookInfo (){
+    try{
+        const response = await fetch('https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books/:bookId')
+        const result = await response.json();
+    } catch(e){
+        console.error(e);
+        console.error(`Failed to get book of ${Id}`)
+    }
+}
+
+export default AddAllBooks;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,1 +1,20 @@
 /* TODO - add your code to create a functional React component that renders a login form */
+import React from "react"
+
+function LogInForm() {
+    return(
+        <>
+        <div>
+            <label>Email:</label>
+            <input type="email" placeholder="Email Login"></input>
+            <label>Password:</label>
+            <input type="password" placeholder="Password"></input>
+            <button type="submit">Submit</button>
+        </div>
+        </>
+
+    )
+}
+
+export default LogInForm;
+LogInForm();
