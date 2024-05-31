22d0ee05c66c574cb3550e8f851d2b6835314db4
Tyler Sloan <tcsloan44@gmail.com>
2024-05-30 23:31:04 -0400

    added states to the input values of the Login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,12 @@
 /* TODO - add your code to create a functional React component that renders a login form */
 import React from "react"
+import { useState } from "react";
 
 export default function LogInForm() {
+    const [userEmail, setUserEmail] = useState("");
+    const [userPassword, setUserPassword] = useState("");
+
+
     function handleSubmit(e) {
         e.preventDefault();
         const form = e.target;
@@ -25,9 +30,23 @@ export default function LogInForm() {
             <div>
                 <form method="post" onSubmit={handleSubmit}>
                     <label>Email:</label>
-                    <input type="email" name="userEmail" placeholder="Email Login"/>
+                    <input 
+                        type="email" 
+                        name="userEmail" 
+                        value={userEmail}
+                        onChange={(e)=>{
+                            setUserEmail(e.target.value);
+                        }} 
+                        placeholder="Email Login" />
                     <label>Password:</label>
-                    <input type="password" name="userPassword" placeholder="Password"></input>
+                    <input 
+                        type="password" 
+                        name="userPassword" 
+                        onChange={(e)=>{
+                            setUserPassword(e.target.value)
+                        }}
+                        placeholder="Password">
+                    </input>
                     <button type="submit">Submit</button>
                 </form>
             </div>
